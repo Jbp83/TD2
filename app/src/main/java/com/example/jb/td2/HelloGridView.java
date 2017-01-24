8c06e077f644f1b55bd8ad34d7e027f3fc49954d
PONZO_Jb <jeanbaptiste.ponzo@gmail.com>
2017-01-24 16:34:12 +0100

    pleins de changements

---
+++ app/src/main/java/com/example/jb/td2/HelloGridView.java
@@ -0,0 +1,31 @@
+package com.example.jb.td2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+/**
+ * Created by Jb on 24/01/2017.
+ */
+
+public class HelloGridView extends AppCompatActivity{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(HelloGridView.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
